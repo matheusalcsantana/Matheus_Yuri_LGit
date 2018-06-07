059f1677a5e3adbd026b026d535e5a3f2466ea90
117211246 YURI SOUZA DOS SANTOS <yuriss@lcc3-077.lcc.ufcg.edu.br>
2018-06-07 11:49:34 -0300

    Modifiquei a classe Animal e Estadia

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -2,4 +2,42 @@ package hotel;
 
 public class Animal {
 
+	public String nome;
+	public String tipo;
+	public int idade;
+	
+	
+	public Animal(String nome, String tipo, int idade) {
+		if (nome.isEmpty() || nome == null) {
+			throw new IllegalArgumentException("Nome de um animal nao pode ser nulo ou vazio");
+		}
+		if (tipo.isEmpty() || tipo == null) {
+			throw new IllegalArgumentException("Tipo de um animal nao pode ser nulo ou vazio");
+		}
+		if (idade < 0) {
+			throw new ArithmeticException("Idade de um animal nao pode ser abaixo de 0");
+		}
+		
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	
 }
---
+++ src/hotel/Estadia.java
@@ -0,0 +1,47 @@
+package hotel;
+
+public class Estadia {
+
+	public Animal animal;
+	public double valor;
+	public int dias;
+	
+	
+	public Estadia(String nome, String tipo, int idade, double valor, int dias) {
+		
+		this.animal = new Animal(nome, tipo, idade);
+		this.valor = valor;
+		this.dias = dias;
+		
+	}
+
+
+	public double getValor() {
+		return valor;
+	}
+
+
+	public int getDias() {
+		return dias;
+	}
+	
+	public String getNome() {
+		return animal.getNome();
+	}
+
+
+	public String getTipo() {
+		return animal.getTipo();
+	}
+
+
+	public int getIdade() {
+		return animal.getIdade();
+	}
+	
+	public String toString() {
+		return this.getNome() + ", " + this.getTipo() + ", " + this.getDias() + " com o preço de R$ " + getValor();
+
+	}
+	
+}
